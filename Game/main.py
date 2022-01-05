d1e6c5408d22176a3777e85f2009f2bca34b2758
Jacob <simerly81@gmail.com>
2022-01-05 18:23:14 -0500

    Started on player turns

--- Game/main.py
+++ Game/main.py
@@ -5,15 +5,17 @@ WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption('Dominion')
 
 WHITE = (255, 255, 255)
+FPS = 30
 
 def draw_window():
     WIN.fill(WHITE)
     pygame.display.update()
 
 def main():
+    #clock = pygame.time.Clock()
     run = True
-
     while run:
+        #clock.tick(FPS)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
